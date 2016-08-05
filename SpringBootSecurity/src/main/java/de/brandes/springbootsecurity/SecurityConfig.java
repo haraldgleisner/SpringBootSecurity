5a5f496b4f4373710acea0c835c5ae4e3b9b2ade
brandes <brandes@brandes-PC.fritz.box>
2016-08-05 16:02:20 +0200

    Freigabe 1

---
+++ SpringBootSecurity/src/main/java/de/brandes/springbootsecurity/SecurityConfig.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.brandes.springbootsecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+/**
+ * Security configuration.
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com).
+ */
+@Configuration
+@EnableWebMvcSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("secret1").roles("USER")
+                .and()
+                .withUser("user2").password("secret2").roles("USER");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated();
+        http.httpBasic();
+        http.csrf().disable();
+    }
+}
---
+++ SpringBootSecurity/src/main/java/de/brandes/springbootsecurity/UserService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.brandes.springbootsecurity;
+/**
+ *
+ * @author brandes
+ */
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Sample service.
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com).
+ */
+@RestController
+@RequestMapping("/service")
+public class UserService {
+    @RequestMapping(value = "/echo/{in}", method = RequestMethod.GET)
+    public String echo(@PathVariable(value = "in") final String in) {
+        return "You said: " + in;
+    }
+}
