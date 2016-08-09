ed48d7c458ca2ad599be2899bf924ab3a5e8f27c
gleisner <harald.gleisner@googlemail.com>
2016-08-09 20:05:16 +0200

    BranchA

--- SpringBootSecurity/src/main/java/de/brandes/springbootsecurity/SecurityConfig.java
+++ SpringBootSecurity/src/main/java/de/brandes/springbootsecurity/SecurityConfig.java
@@ -9,11 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-/**
- * Security configuration.
- *
- * @author Udo Brandes
- */
 @Configuration
 @EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
